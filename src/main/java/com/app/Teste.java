f1e6372deef4d4dde3ab7a4a11516cd096d739bd
devluanna <luanagcontato@gmail.com>
2024-08-27 12:35:35 -0300

    first commit: infra

---
+++ src/main/java/com/app/Teste.java
@@ -0,0 +1,8 @@
+package com.app;
+
+public class Teste {
+    public static void main(String[] args) {
+        System.out.print("Teste");
+
+    }
+}
---
+++ src/test/java/com/app/wishly_app/WishlyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wishly_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlyAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
